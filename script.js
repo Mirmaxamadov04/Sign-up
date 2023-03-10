81bb6ea6e303e26e7b6016ca881ae34fbc99b6cb
Mirmaxamadov04 <d@mirmaxamadov.gmail.com>
2023-03-10 16:05:25 +0500

    done

--- script.js
+++ script.js
@@ -3,18 +3,70 @@ elMainLeft = document.querySelector(".main__left");
 elButtonRight = document.querySelector(".main__right__button");
 elMainRight = document.querySelector(".main__right");
 elSignUp = document.querySelector(".sign-up");
+elLogin = document.querySelector(".login");
+elGoBack = document.querySelector(".go-back");
+elResBtnSign = document.querySelector(".res-btn-sign");
+elResBtnLogin = document.querySelector(".res-btn-login ");
 
-//events
+const toconvertform = function (mainLeft, mainRight, login, signUp, goBack) {
+  mainLeft.style.opacity = "0";
+  mainRight.style.opacity = "1";
+  login.style.display = "none";
+  signUp.style.display = "block";
+  goBack.classList.toggle("signclicked");
+};
+
 elButtonRight.addEventListener("click", (evt) => {
   evt.preventDefault();
-  elMainLeft.style.opacity = "1";
-  elMainRight.style.opacity = "0";
-  elSignUp.classList.toggle("sign-up-clicked");
+  toconvertform(elMainRight, elMainLeft, elSignUp, elLogin, elGoBack);
+  elGoBack.classList.remove("loginclicked");
 });
 
 elButtonLeft.addEventListener("click", (evt) => {
   evt.preventDefault();
-  elMainRight.style.opacity = "1";
-  elMainLeft.style.opacity = "0";
-  elSignUp.classList.toggle("sign-up-clicked");
+  toconvertform(elMainLeft, elMainRight, elLogin, elSignUp, elGoBack);
+  elGoBack.classList.add("loginclicked");
 });
+
+elResBtnSign.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  toconvertform(elMainLeft, elMainRight, elLogin, elSignUp, elGoBack);
+});
+
+elResBtnLogin.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  toconvertform(elMainRight, elMainLeft, elSignUp, elLogin, elGoBack);
+});
+
+// elSignUpHeading = document.querySelector(".sign-up__heading");
+// elSignUpInput = document.querySelector("#email");
+// elSignUpButton = document.querySelector(".sign-up__button");
+
+// elLogin = document.querySelector(".login");
+
+//events
+// elButtonRight.addEventListener("click", (evt) => {
+//   evt.preventDefault();
+
+//   elSignUp.classList.toggle("clicked");
+//   setTimeout(() => {
+//     elMainLeft.style.opacity = "1";
+//     elMainRight.style.opacity = "0";
+//     elSignUpHeading.textContent = "Login";
+//     elSignUpInput.style.display = "none";
+//     elSignUpButton.textContent = "Login";
+//   }, 800);
+// });
+
+// elButtonLeft.addEventListener("click", (evt) => {
+//   evt.preventDefault();
+
+//   setTimeout(() => {
+//     elSignUp.classList.toggle("clicked");
+//     elMainRight.style.opacity = "1";
+//     elMainLeft.style.opacity = "0";
+//     elSignUpHeading.textContent = "Sign up";
+//     elSignUpInput.style.display = "block";
+//     elSignUpButton.textContent = "Sign up";
+//   }, 400);
+// });
